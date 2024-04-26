8bc64331f1fe622b66b4086e60a0fed0d883698a
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-04-26 19:11:18 +0530

    dev: sidebar const

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        label: "Home",
+        route: "/",
+        imgUrl: '/icon/home.svg'
+    },
+    {
+        label: "Upcoming",
+        route: "upcoming",
+        imgUrl: '/icon/upcoming.svg'
+    },
+    {
+        label: "Previous",
+        route: "/previous",
+        imgUrl: '/icon/previous.svg'
+    },
+    {
+        label: "Recordings",
+        route: "/recordings",
+        imgUrl: '/icon/video.svg'
+    },
+    {
+        label: "Personal Room",
+        route: "/personal-room",
+        imgUrl: '/icon/add-personal.svg'
+    },
+]
