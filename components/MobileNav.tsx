e4000887ded62c84d95ddebed62ed01557938aa2
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-04-30 19:16:00 +0530

    bug: mobile navbar error

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -22,7 +22,7 @@ export default function MobileNav() {
             className="cursor-pointer"
           />
         </SheetTrigger>
-        <SheetContent side="left" className="border-none bg-dark-1">
+        <SheetContent side="right" className="border-none bg-dark-1">
           <Link href="/" className="flex items-center gap-1">
             <Image
               src="/icons/logo.svg"
