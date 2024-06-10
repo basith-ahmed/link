1e10d3bdb2212e8c0aa1c806c52c9dcaac3862a5
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-06-10 05:55:25 +0530

    schedule meeting

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -5,19 +5,19 @@ import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 interface HomeCardProps {
-  color?: string;
+  className?: string;
   img: string;
   title: string;
   description: string;
   handleClick?: () => void;
 }
 
-const HomeCard = ({ color, img, title, description, handleClick }: HomeCardProps) => {
+const HomeCard = ({ className, img, title, description, handleClick }: HomeCardProps) => {
   return (
     <section
       className={cn(
         'bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer',
-        color
+        className
       )}
       onClick={handleClick}
     >
